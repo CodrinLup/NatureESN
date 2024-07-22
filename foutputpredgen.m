a2f4de37779d4e5c3e3d64f6a405f20205f2cc6f
CodrinLup <codrin-alexandru.lupascu@academic.tuiasi.ro>
2024-07-22 18:49:32 +0300

    Delete foutputpredgen.m

--- foutputpredgen.m
+++
@@ -1,8 +0,0 @@
-function outputpred = foutputpredgen(Cfnetwork,statepred)
-
-[xlen, len] = size(statepred);
-outputpred = zeros(len,1);
-for i=2:len
-    outputpred(i) = Cfnetwork*statepred(:,i);
-end
-end
